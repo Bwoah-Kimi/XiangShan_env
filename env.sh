88925e72fa8b19d8e591c0e357ef2dd9b6989a8b
Bwoah-Kimi <jameszhu.20021122@gmail.com>
2026-02-04 14:54:01 +0800

    misc: update env.sh

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 # This script will setup XiangShan environment variables
 
 export XS_PROJECT_ROOT=$(pwd)
 export NEMU_HOME=$(pwd)/NEMU
 export AM_HOME=$(pwd)/nexus-am
-#export NOOP_HOME=$(pwd)/XiangShan-kunminghu
-#export NOOP_HOME=$(pwd)/XiangShan-southlake
-export NOOP_HOME=$(pwd)/XiangShan_Mar08
+export NOOP_HOME=$(pwd)/XiangShan
 export DRAMSIM3_HOME=$(pwd)/DRAMsim3
 
 echo SET XS_PROJECT_ROOT: ${XS_PROJECT_ROOT}
